f7aaebbc9d717eadc814a9affbc902d1b53a4eab
DheerajAlwala <dheeraj.alwala09@gmail.com>
2021-07-01 23:34:13 +0530

    ver 1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyC4u9LFE_Htz1EZK4ybgkiSN2RJrJaYQAA",
+    authDomain: "olorder-f8a5f.firebaseapp.com",
+    databaseURL: "https://olorder-f8a5f-default-rtdb.firebaseio.com",
+    projectId: "olorder-f8a5f",
+    storageBucket: "olorder-f8a5f.appspot.com",
+    messagingSenderId: "103370810500",
+    appId: "1:103370810500:web:1fba38ad5c2eaedeeb0ed1",
+    measurementId: "G-Z8XRE4N2EV"
+  }
+};
